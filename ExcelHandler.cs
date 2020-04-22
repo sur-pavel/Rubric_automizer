5750e6e244ee2c53cf3ed7c368a75e2d6e1d157d
Павел Сурков <sur-pavel@mail.ru>
2020-04-22 22:28:32 +0300

    Replace UsingInteropExcel() with GetSubtitlesObjs()

--- ExcelHandler.cs
+++ ExcelHandler.cs
@@ -7,76 +7,81 @@ namespace Rubric_automizer
     internal class ExcelHandler
     {
         private const string EXCEL_FILE_PATH = @"c:\Users\sur-p\Downloads\Default_Rubrics.xlsx";
-        internal List<SubtitleObj> subtitlesObjs;
 
         internal List<SubtitleObj> GetSubtitlesObjs()
         {
-            subtitlesObjs = new List<SubtitleObj>();
-            UsingInteropExcel();
-            return subtitlesObjs;
-        }
-
-        private void UsingInteropExcel()
-        {
-            Excel.Application app = new Excel.Application();
-            Excel.Workbook workbook = app.Workbooks.Open(EXCEL_FILE_PATH);
-            Excel.Worksheet worksheet = (Excel.Worksheet)workbook.Sheets[1];
-            Excel.Range lastCell = worksheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);
-            int lastColumn = (int)lastCell.Column;
-            int lastRow = (int)lastCell.Row;
-            string text = "";
-            string firstTitle = "";
-            string title = "";
-            string subtitle = "";
-            string index_MDA = "";
-
-            for (int rowIndex = 1; rowIndex <= lastRow; rowIndex++)
+            List<SubtitleObj> subtitlesObjs = new List<SubtitleObj>();
+            try
             {
-                for (int columnIndex = 1; columnIndex <= lastColumn; columnIndex++)
+                Console.WriteLine("Getting subtitles from Excel begin");
+
+                Excel.Application app = new Excel.Application();
+                Excel.Workbook workbook = app.Workbooks.Open(EXCEL_FILE_PATH);
+                Excel.Worksheet worksheet = (Excel.Worksheet)workbook.Sheets[1];
+                Excel.Range lastCell = worksheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);
+                int lastColumn = (int)lastCell.Column;
+                int lastRow = (int)lastCell.Row;
+                string text = "";
+                string firstTitle = "";
+                string title = "";
+                string subtitle = "";
+                string index_MDA = "";
+
+                for (int rowIndex = 1; rowIndex <= lastRow; rowIndex++)
                 {
-                    text = worksheet.Cells[rowIndex, columnIndex].Text.ToString();
-                    if (String.IsNullOrEmpty(text))
+                    for (int columnIndex = 1; columnIndex <= lastColumn; columnIndex++)
                     {
-                        continue;
-                    }
-
-                    if (columnIndex > 1)
-                    {
-                        title = worksheet.Cells[rowIndex, columnIndex - 1].Text.ToString();
-                        int inc = 1;
-                        while (String.IsNullOrEmpty(title))
+                        text = worksheet.Cells[rowIndex, columnIndex].Text.ToString();
+                        if (String.IsNullOrEmpty(text))
                         {
-                            title = worksheet.Cells[rowIndex - inc, columnIndex - 1].Text.ToString();
-                            inc++;
+                            continue;
                         }
-                    }
-                    else
-                    {
-                        title = firstTitle;
-                    }
 
-                    if (text.Contains("Предметный заголовок"))
-                    {
-                        int splitLength = text.Split('|').Length;
-                        firstTitle = text.Split('|')[splitLength - 1];
-                        index_MDA = text.Split('|')[1];
-                        Console.WriteLine("\n\nдобавлен Индекс: " + index_MDA + ", Предметный заголовок: " + firstTitle);
-                        title = firstTitle;
-                        subtitle = firstTitle;
-                    }
-                    else
-                    {
-                        subtitle = text;
-                    }
+                        if (columnIndex > 1)
+                        {
+                            title = worksheet.Cells[rowIndex, columnIndex - 1].Text.ToString();
+                            int inc = 1;
+                            while (String.IsNullOrEmpty(title))
+                            {
+                                title = worksheet.Cells[rowIndex - inc, columnIndex - 1].Text.ToString();
+                                inc++;
+                            }
+                        }
+                        else
+                        {
+                            title = firstTitle;
+                        }
 
-                    SubtitleObj subtitleObj = new SubtitleObj(index_MDA, title, subtitle);
-                    Console.WriteLine("ИНДЕКС МДА: " + index_MDA + " ЗАГОЛОВОК: " + subtitleObj.Title + " ПОДЗАГОЛОВОК: " + subtitleObj.Subtitle);
+                        if (text.Contains("Предметный заголовок"))
+                        {
+                            int splitLength = text.Split('|').Length;
+                            firstTitle = text.Split('|')[splitLength - 1];
+                            index_MDA = text.Split('|')[1];
+                            Console.WriteLine("\n\nдобавлен Индекс: " + index_MDA + ", Предметный заголовок: " + firstTitle);
+                            title = firstTitle;
+                            subtitle = firstTitle;
+                        }
+                        else
+                        {
+                            subtitle = text;
+                        }
 
-                    subtitlesObjs.Add(subtitleObj);
+                        SubtitleObj subtitleObj = new SubtitleObj(index_MDA, title, subtitle);
+                        //Console.WriteLine("ИНДЕКС МДА: " + index_MDA + " ЗАГОЛОВОК: " + subtitleObj.Title + " ПОДЗАГОЛОВОК: " + subtitleObj.Subtitle);
+
+                        subtitlesObjs.Add(subtitleObj);
+                    }
                 }
+                workbook.Close(false);
+                app.Quit();
+                Console.WriteLine("Getting subtitles from Excel ended");
             }
-            workbook.Close(false);
-            app.Quit();
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return subtitlesObjs;
         }
     }
 }
