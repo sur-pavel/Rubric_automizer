727df54a714d0b6d9ec6975ca782fe39b68e0761
Павел Сурков <sur-pavel@mail.ru>
2020-04-22 17:22:54 +0300

    modify Drop and Create tables.sql

--- sql/Drop and Create tables.sql
+++ sql/Drop and Create tables.sql
@@ -1,11 +1,28 @@
-while (String.IsNullOrEmpty(index_MDA) || String.IsNullOrWhiteSpace(index_MDA))
-                        {
-                            Console.WriteLine($"MFN: {mfn}");
-                            Console.WriteLine("ИНДЕКС МДА: " + index_MDA + " ЗАГОЛОВОК: " + subtitleObj.Title + " ПОДЗАГОЛОВОК: " + subtitleObj.Subtitle + "\n");
-                            
-                        }
-						
-						
-						
-						
-						fieldText.Split('^')[1].Substring(1)
+DROP TABLE IF EXISTS doc_subtitles
+CASCADE;
+
+CREATE TABLE doc_subtitles
+(
+    doc_subtitle_id SERIAL PRIMARY KEY,
+	index_MDA VARCHAR(4),
+    title VARCHAR(200),
+    subtitle VARCHAR(200)
+);
+
+DROP TABLE IF EXISTS spell_dictionary
+CASCADE;
+
+CREATE TABLE spell_dictionary
+(
+    word_id SERIAL PRIMARY KEY,	
+    word VARCHAR(200)
+);
+
+DROP TABLE IF EXISTS wrong_subtitles
+CASCADE;
+
+CREATE TABLE wrong_subtitles
+(
+    wrong_subtitle_id SERIAL PRIMARY KEY,
+    subtitle VARCHAR(200)
+);
