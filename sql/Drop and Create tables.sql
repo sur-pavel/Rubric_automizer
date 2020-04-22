1352d14d7434bcd2597101170dae34efc3b275c4
Павел Сурков <sur-pavel@mail.ru>
2020-04-22 17:18:21 +0300

    new file

---
+++ sql/Drop and Create tables.sql
@@ -0,0 +1,11 @@
+while (String.IsNullOrEmpty(index_MDA) || String.IsNullOrWhiteSpace(index_MDA))
+                        {
+                            Console.WriteLine($"MFN: {mfn}");
+                            Console.WriteLine("ИНДЕКС МДА: " + index_MDA + " ЗАГОЛОВОК: " + subtitleObj.Title + " ПОДЗАГОЛОВОК: " + subtitleObj.Subtitle + "\n");
+                            
+                        }
+						
+						
+						
+						
+						fieldText.Split('^')[1].Substring(1)
