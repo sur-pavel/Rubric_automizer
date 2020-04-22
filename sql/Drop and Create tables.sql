d6d5eee9e2c702644019762499912cedc1bff75e
Павел Сурков <sur-pavel@mail.ru>
2020-04-22 22:25:35 +0300

    add UNIQUE and doc_subtitles_wrong_subtitles

--- sql/Drop and Create tables.sql
+++ sql/Drop and Create tables.sql
@@ -15,7 +15,8 @@ CASCADE;
 CREATE TABLE spell_dictionary
 (
     word_id SERIAL PRIMARY KEY,	
-    word VARCHAR(200)
+    word VARCHAR(200),
+	UNIQUE (word)
 );
 
 DROP TABLE IF EXISTS wrong_subtitles
@@ -24,5 +25,19 @@ CASCADE;
 CREATE TABLE wrong_subtitles
 (
     wrong_subtitle_id SERIAL PRIMARY KEY,
-    subtitle VARCHAR(200)
+    subtitle VARCHAR(200),
+	UNIQUE (subtitle)
 );
+
+DROP TABLE IF EXISTS doc_subtitles_wrong_subtitles
+CASCADE;
+
+CREATE TABLE doc_subtitles_wrong_subtitles
+(
+    doc_subtitle_id int NOT NULL,
+    wrong_subtitle_id int NOT NULL,
+    PRIMARY KEY (doc_subtitle_id, wrong_subtitle_id),
+    FOREIGN KEY (doc_subtitle_id) REFERENCES doc_subtitles,
+    FOREIGN KEY (wrong_subtitle_id) REFERENCES wrong_subtitles
+);
+
